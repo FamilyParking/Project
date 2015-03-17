e0627c6e4ba289488fe66e7e4ae7225591f706a7
ObaidaHanteer <Obaida.Hanteer@hotmail.com>
2015-03-17 02:04:09 +0100

    3-17-2015

--- Android/FamilyParking2/app/src/main/java/it/familiyparking/app/Widget/FamilyParkingWidgetProvider.java
+++ Android/FamilyParking2/app/src/main/java/it/familiyparking/app/Widget/FamilyParkingWidgetProvider.java
@@ -8,35 +8,68 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
+import com.google.android.gms.analytics.Tracker;
+
+import java.util.ArrayList;
+
 import it.familiyparking.app.MainActivity;
 import it.familiyparking.app.R;
+import it.familiyparking.app.dao.CarTable;
+import it.familiyparking.app.dao.UserTable;
+import it.familiyparking.app.serverClass.Car;
+import it.familiyparking.app.serverClass.User;
+import it.familiyparking.app.task.DoPark;
+import it.familiyparking.app.utility.Tools;
 
 public class FamilyParkingWidgetProvider extends AppWidgetProvider {
 
     private static final String Park_Button_Clicked_Action = "park_button_clicked";
     private static final String UnPark_Button_Clicked_Action = "unpark_button_clicked";
-
+    private  User user ;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
         super.onUpdate(context,appWidgetManager,appWidgetIds);
-        /*int[] appWidgetIds holds ids of multiple instance
-        * of your widget meaning you are placing more than one widgets on  your home screen*/
+     /*int[] appWidgetIds holds ids of multiple instance
+      * of your widget meaning you are placing more than one widgets on  your home screen*/
         final int N = appWidgetIds.length;
         for (int i=0; i<N ; i++)
         {
-            RemoteViews remoteView = new RemoteViews(context.getPackageName(), R.layout.widget);
-            //Assign a behaviour to the button according to the number of the cars
-            assignBehaviourToButtons(context,remoteView);
+            RemoteViews remoteView = new RemoteViews(context.getPackageName(),R.layout.widget);
+            if(hasAnAccount(context)) {
+                remoteView.setViewVisibility(R.id.buttonsLayout, View.INVISIBLE);
+                remoteView.setViewVisibility(R.id.noCarsLayout,View.INVISIBLE);
+                remoteView.setViewVisibility(R.id.signInLayout,View.VISIBLE);
+            }
+            else {
+                //Check the number of cars the current user share
+                int numOfCars = countCarsCurrentUserSharing(context);
+                if(numOfCars==0)
+                {
+                    // in this case tell the user that he has no shared yet
+                    remoteView.setViewVisibility(R.id.buttonsLayout, View.INVISIBLE);
+                    remoteView.setViewVisibility(R.id.noCarsLayout,View.VISIBLE);
+                    remoteView.setViewVisibility(R.id.signInLayout,View.INVISIBLE);
+                }
+                else {
+                    //Show the parking and unparking buttons
+                    remoteView.setViewVisibility(R.id.buttonsLayout, View.VISIBLE);
+                    remoteView.setViewVisibility(R.id.noCarsLayout,View.INVISIBLE);
+                    remoteView.setViewVisibility(R.id.signInLayout, View.INVISIBLE);
+                    //Assign a behaviour to the button according to the number of the cars
+                    assignBehaviourToButtons(context, remoteView,numOfCars);
+                }
+            }
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetIds[i], remoteView);
         }
     }
-
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
@@ -56,35 +89,33 @@ public class FamilyParkingWidgetProvider extends AppWidgetProvider {
 
         super.onDeleted(context,appWidgetIds);
 
-        Toast.makeText(context,"see ya sucka",Toast.LENGTH_SHORT).show();
+        Toast.makeText(context,"Why did you do that ? :( ",Toast.LENGTH_SHORT).show();
 
     }
-
     protected PendingIntent getPendingSelfIntent(Context context, String action) {
         Intent intent = new Intent(context, getClass());
         intent.setAction(action);
         return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
-
-    protected int countCarsCurrentUserSharing()
+    protected int countCarsCurrentUserSharing(Context context)
     {
-        return 2;
+        SQLiteDatabase db = Tools.getDB_Readable(context);
+        final ArrayList<Car> cars = CarTable.getAllCar(db);
+        db.close();
+        return  cars.size();
     }
-
-    protected void assignBehaviourToButtons(Context context, RemoteViews remoteView) {
-        if (countCarsCurrentUserSharing() > 1) {
+    //Assign the behaviour of the buttons according to the number of cars this user has
+    protected void assignBehaviourToButtons(Context context, RemoteViews remoteView, int numOfCars) {
+        if (numOfCars > 1) {
             // Create an Intent to launch ExampleActivity
-            Intent intent = new Intent(context,MainActivity.class);
+            Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
             // Get the layout for the App Widget and attach an on-click listener
             remoteView.setOnClickPendingIntent(R.id.park_button, pendingIntent);
             remoteView.setOnClickPendingIntent(R.id.unpark_button, pendingIntent);
 
-
-
-
-        } else if (countCarsCurrentUserSharing() == 1) {
+        } else if (numOfCars == 1) {
             remoteView.setOnClickPendingIntent(R.id.park_button,
                     getPendingSelfIntent(context, Park_Button_Clicked_Action));
             remoteView.setOnClickPendingIntent(R.id.unpark_button,
@@ -92,6 +123,18 @@ public class FamilyParkingWidgetProvider extends AppWidgetProvider {
         }
     }
 
+    //Returns true if the user has an account in FamilyParking DB
+    protected boolean hasAnAccount(Context context)
+    {
+        SQLiteDatabase db = Tools.getDB_Readable(context);
+        user = UserTable.getUser(db);
+        db.close();
+        if(user==null)
+        {
+            return false ;
+        }
+        return true;
+    }
     protected void doUnpraking()
     {
 
@@ -100,5 +143,4 @@ public class FamilyParkingWidgetProvider extends AppWidgetProvider {
     {
 
     }
-
 }
